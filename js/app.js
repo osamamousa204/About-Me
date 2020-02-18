8feef38a70cc54a397039cba971fa45a2900966b
osamamousa204 <osamamousa204@gmail.com>
2020-02-18 21:49:41 +0200

    c

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ console.log(spacialty);
 var result = document.getElementById("gis");
 console.log(result);
 
-result.innerHTML =  myAge +   "    "  + '<br>'+ mycolor + "    "  + '<br>' + spacialty  ;
 
+var mark = 0;
 var sky = prompt("do i love science answer(yes/no) or (y/n)");
 console.log(sky);
 
@@ -20,6 +20,7 @@ switch (sky.toLowerCase()){
     case 'yes' :
      case 'y' :
     alert('you are right i love it so much');
+    mark = mark + 1;
     break;
     case 'no' :
     case 'n' :
@@ -31,16 +32,21 @@ switch (sky.toLowerCase()){
     default :
  alert("you didn't type a correct answer!");
 }
+// console.log(mark);
 var phi = prompt("Is phisics what i love the most in science? answer(yes/no) or (y/n)");
 switch(phi.toLowerCase()){
  case "yes" :
     alert('you are right ');
+    mark = mark + 1;
+
     break;
  case "no" :
      alert("you are wrong!!");
      break;
      case "y" :
     alert('you are right ');
+    mark = mark + 1;
+
     break;
  case "n" :
      alert("you are wrong!!");
@@ -48,33 +54,47 @@ switch(phi.toLowerCase()){
  default :
  alert("you didn't type a correct answer!");
 }
+// console.log(mark);
+
 var zero = prompt("do i play pes now ? answer(yes/no) or (y/n)");
 switch(zero.toLowerCase()){
  case "yes" :
     alert('you are wrong i was playing it sadly i can not now ');
+
     break;
  case "no" :
      alert("you are right!!");
+     mark = mark + 1;
+
      break;
  case "y" :
     alert('you are wrong i was playing it sadly i can not now');
+
     break;
  case "n" :
      alert("you are right!!");
+     mark = mark + 1;
+
      break;
  default :
  alert("you didn't type a correct answer!");
 }
+// console.log(mark);
+
 var hungry = prompt("am i hungry? answer(yes/no) or (y/n)");
 switch(hungry.toLowerCase()){
  case "yes" :
     alert("you are right!!");
+    mark = mark + 1;
+
     break;
  case "no" :
      alert("you are wrong!!");
      break;
      case "y" :
     alert("you are right!!");
+    mark = mark + 1;
+
     break;
  case "n" :
      alert("you are wrong!!");
@@ -82,17 +102,22 @@ switch(hungry.toLowerCase()){
  default :
  alert("you didn't type a correct answer!");
 }
+// console.log(mark);
 
 var life = prompt("do i love mansaf? answer(yes/no) or (y/n)");
 switch(life.toLowerCase()){
  case "yes" :
     alert("you are right!! who hates mansaf");
+    mark = mark + 1;
+
     break;
  case "no" :
      alert(" sure you are wrong !  ");
      break;
      case "y" :
         alert("you are right!! who hates mansaf");
+        mark = mark + 1;
+
         break;
      case "n" :
          alert("sure you are wrong ! ");
@@ -101,4 +126,59 @@ switch(life.toLowerCase()){
  alert("you didn't type a correct answer!");
 }
 
-alert("thank you to vist my website " + Name)
+console.log(mark);
+var myNumber = 8 ;
+var guessNumber = prompt("guess an even number from 0 to 10 (you have only 4 attempts)");
+for(var i = 1 ; i < 6 ; i++){
+
+   if(myNumber == guessNumber){
+     mark = mark + 1;
+      alert("your answer " + guessNumber + " is right");
+      break;
+   }
+
+      else if(guessNumber < myNumber){
+         alert('your answer too low ')
+         guessNumber = prompt("guess an even number from 0 to 10 (you have only " + (4 - i) + " attempts)");
+
+      }
+      else if (guessNumber > myNumber){
+         alert('your answer too high ')
+         guessNumber = prompt("guess an even number from 0 to 10 (you have only " + (4 - i) + " attempts)");
+
+      }else{
+         alert('your answer is wrong ');
+         guessNumber = prompt("guess an even number from 0 to 10 (you have only " + (4 - i) + " attempts)");
+      }
+      
+
+
+   }
+   console.log(mark);
+
+   var guessFav = ['razan', 'shihab' , 'baraa' ,'samer', 'bashar' , 'riham' , 'all' ];
+   
+   var userfav = prompt("guess my favourit instructor (you have only 6 attepts) ");
+   for(var i = 1 ; i < 8 ; i++){
+      for(var x = 0; x < guessFav.length;x++){
+         if(userfav == guessFav[x]){
+            mark = mark + 1
+            alert("yes but she/he is not the only one");
+            var test = "y";
+         }
+      }
+      x = 0;
+      if(test == "y"){
+         break;
+      }
+      userfav = prompt("guess my favourit instructor you only have " + (6 -i) + " attempts" );  
+   
+
+
+      }
+   
+      console.log(mark);
+
+   alert("thank you to vist my website " +  Name + " and you have " + mark + " out of 7 ")
+
+   result.innerHTML =  myAge +   "    "  + '<br>'+ mycolor + "    "  + '<br>' + spacialty + '<br>' + "Your mark is : " + mark + " out of 7"  ;
